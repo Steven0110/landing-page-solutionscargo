bc59dfb5e4cadb0186bf2eabfb3fc3e9ec900879
Steven <cabello.acosta.gerardo@gmail.com>
2023-07-24 19:13:49 -0600

    Social netowks added. Whatsapp integration

--- src/components/Social/index.tsx
+++ src/components/Social/index.tsx
@@ -9,16 +9,16 @@ export default function Social() {
 			<div className="social__title text-center mb-4 md:mb-6">Síguenos en nuestras redes sociales</div>
 			<div className="social__text text-center mb-8 md:mb-24">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus tempora beatae eos perspiciatis mollitia odit impedit animi, optio. Doloremque ipsam quas cumque, nostrum. Amet corrupti magnam recusandae alias sit nostrum!</div>
 			<div className="social__icons">
-				<Link href="#" className="social__icon ml">
+				<Link href="https://www.linkedin.com/company/solutions-cargo-s-a-de-c-v/about/" className="social__icon ml" target="_blank">
 					<i className="pi pi-linkedin text-5xl md:text-6xl mx-2 md:mx-8"></i>
 				</Link>
-				<Link href="#" className="social__icon">
+				<Link href="https://api.whatsapp.com/send?phone=525517948196&text=Hola%2C%20me%20gustar%C3%ADa%20conocer%20m%C3%A1s." className="social__icon" target="_blank">
 					<i className="pi pi-whatsapp text-5xl md:text-6xl mx-2 md:mx-8"></i>
 				</Link>
-				<Link href="#" className="social__icon">
+				<Link href="https://www.facebook.com/SOLUTIONSCARGO/?locale=es_LA" className="social__icon" target="_blank">
 					<i className="pi pi-facebook text-5xl md:text-6xl mx-2 md:mx-8"></i>
 				</Link>
-				<Link href="#" className="social__icon">
+				<Link href="https://www.instagram.com/solutions_cargo/" className="social__icon" target="_blank">
 					<i className="pi pi-instagram text-5xl md:text-6xl mx-2 md:mx-8"></i>
 				</Link>
 			</div>
