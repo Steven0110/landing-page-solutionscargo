a5021aa017ccc488909687eed8883ed37daeb1ff
Steven <cabello.acosta.gerardo@gmail.com>
2023-07-24 19:08:45 -0600

    Favicon added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
+      <link rel="icon" href="/favicon.png" type="image/x-icon" />
+
+      <link rel="apple-touch-icon" href="/favicon.png" />
+      <link rel="apple-touch-icon" sizes="76x76" href="/favicon.png" />
+      <link rel="apple-touch-icon" sizes="120x120" href="/favicon.png" />
+      <link rel="apple-touch-icon" sizes="152x152" href="/favicon.png" />
       <body className={inter.className}>{children}</body>
     </html>
   )
